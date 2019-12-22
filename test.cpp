21dd2a4031b529750b163968764e4d18daed5bf4
changxin <cx@uxsino.com>
2019-12-22 13:43:21 +0800

    add function [splitStringToStr:splitStringToInt:stringToInt]by changxin 2019-12-22 01

--- test.cpp
+++ test.cpp
@@ -2,7 +2,25 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <sstream>
 using namespace std;
+/**************************************************************************
+
+Copyright:MySelf
+
+Author: cx
+
+Date:2019-12-16
+
+Description:string to vector
+
+Need include file:
+#include <string>
+#include <vector>
+#include <iostream>
+Note:The parameter is const char *
+
+**************************************************************************/
 int Str2Vect(const char* pSrc, std::vector<std::string> &vDest, const char *pSep) {
     if (NULL == pSrc) {
         return -1;
@@ -30,6 +48,110 @@ int Str2Vect(const char* pSrc, std::vector<std::string> &vDest, const char *pSep
     delete[] pTmp;
     return 0;
 }
+/**************************************************************************
+
+Copyright:MySelf
+
+Author: cx
+
+Date:2019-12-22
+
+Description:string to vector<string>
+
+Need include file:
+#include <string>
+#include <vector>
+#include <iostream>
+
+NOTE:The parameter is string
+**************************************************************************/
+bool splitStringToStr(string& strsrc, string& strSplit, vector<string>& vecresult)
+{
+    string sstr = strsrc;
+    if (strsrc.empty())
+    {
+        return false;
+    }
+    string::size_type iStarpos = 0;
+    string::size_type isplitpos = 0;
+    string str;
+    isplitpos = strsrc.find(strSplit,iStarpos);
+    while(string::npos != isplitpos)
+    {
+        str = sstr.substr(iStarpos,isplitpos - iStarpos);
+        vecresult.push_back(str);
+        iStarpos = isplitpos + strSplit.size();
+        isplitpos = strsrc.find(strSplit,iStarpos);
+    }
+    str = strsrc.substr(iStarpos);
+    vecresult.push_back(str);
+    return true;
+}
+/**************************************************************************
+
+Copyright:MySelf
+
+Author: cx
+
+Date:2019-12-22
+
+Description:string to int vector<int>
+
+Need include file:
+#include <string>
+#include <vector>
+#include <iostream>
+
+NOTE:The parameter is string
+**************************************************************************/
+bool splitStringToInt(string &strsrc,string &strSplit,vector<int>& vecresult)
+{
+        string sstr = strsrc;
+        if (strsrc.empty())
+        {
+                return false;
+        }
+        string::size_type iStarpos = 0;
+        string::size_type isplitpos = 0;
+        string str;
+        isplitpos = strsrc.find(strSplit,iStarpos);
+        while(string::npos != isplitpos)
+        {
+                str = sstr.substr(iStarpos,isplitpos - iStarpos);
+                vecresult.push_back(stringToInt(str));
+                iStarpos = isplitpos + strSplit.size();
+                isplitpos = strsrc.find(strSplit,iStarpos);
+        }
+        str = strsrc.substr(iStarpos);
+        vecresult.push_back(stringToInt(str));
+        return true;
+}
+
+/**************************************************************************
+
+Copyright:MySelf
+
+Author: cx
+
+Date:2019-12-22
+
+Description:string to int
+
+Need include file:
+#include <string>
+#include <iostream>
+#include <sstream>
+NOTE:The parameter is string
+**************************************************************************/
+int stringToInt(string str)
+{
+    int res = 0;
+    stringstream ss;
+    ss<<str;
+    ss>>res;
+    return res;
+}
+
 int main()
 {
     const char *str="192.168.0.110";
